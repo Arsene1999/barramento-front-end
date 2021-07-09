1c8b3a91c50f0d5bf666d6baf1196991570f3f0f
João Ferreira dos Santos Neto <joaont.dev@gmail.com>
2021-07-09 16:45:16 -0300

    wip

--- src/pages/LogServicos/index.tsx
+++ src/pages/LogServicos/index.tsx
@@ -1,86 +1,96 @@
-import { Header } from '../../components/Header';
-import {Container, Content, Log} from './style';
+import { Header } from "../../components/Header";
+import { Container, Content, Log } from "./style";
 
-export function LogServicos(){
-    return(
-        <>
-           <Header/>
-           <Container>
-               <Content>
-                   <h1>
-                       Log de serviços
-                   </h1>
-                <Log>
-                        
-                    <table>
-                        <thead>
-                            <tr>
-                                <th>Data</th>
-                                <th>Hora</th>
-                                <th>Emissor</th>
-                                <th>Receptor</th>
-                            </tr>
-                        </thead>
-                        <tbody>
-                            <tr >
-                                <td>18/07/199</td>
-                                <td >
-                                    00:00:00    
-                                </td>
-                                <td>
-                                    João
-                                </td>
-                                <td>
-                                   Andre     
-                    
-                                </td>
-                                </tr>
-                            <tr >
-                                <td>18/07/199</td>
-                                <td >
-                                    00:00:00    
-                                </td>
-                                <td>
-                                    João
-                                </td>
-                                <td>
-                                   Andre     
-                    
-                                </td>
-                                </tr>
-                            <tr >
-                                <td>18/07/199</td>
-                                <td >
-                                    00:00:00    
-                                </td>
-                                <td>
-                                    João
-                                </td>
-                                <td>
-                                   Andre     
-                    
-                                </td>
-                                </tr>
-                            <tr >
-                                <td>18/07/199</td>
-                                <td >
-                                    00:00:00    
-                                </td>
-                                <td>
-                                    João
-                                </td>
-                                <td>
-                                   Andre     
-                    
-                                </td>
-                                </tr>
-                                
-                        </tbody>
-                    </table>
-       
-                </Log>
-               </Content>
-           </Container>
-        </>
-    );
+export function LogServicos() {
+  const logServicos = [
+    {
+      data: "09/07/2021",
+      hora: "16:39",
+      emissor: "João",
+      receptor: "Marcelo",
+    },
+    {
+      data: "09/07/2021",
+      hora: "16:39",
+      emissor: "João",
+      receptor: "Marcelo",
+    },
+    {
+      data: "09/07/2021",
+      hora: "16:39",
+      emissor: "João",
+      receptor: "Marcelo",
+    },
+    {
+      data: "09/07/2021",
+      hora: "16:39",
+      emissor: "João",
+      receptor: "Marcelo",
+    },
+    {
+      data: "09/07/2021",
+      hora: "16:39",
+      emissor: "João",
+      receptor: "Marcelo",
+    },
+    {
+      data: "09/07/2021",
+      hora: "16:39",
+      emissor: "João",
+      receptor: "Marcelo",
+    },
+    {
+      data: "09/07/2021",
+      hora: "16:39",
+      emissor: "João",
+      receptor: "Marcelo",
+    },
+    {
+      data: "09/07/2021",
+      hora: "16:39",
+      emissor: "João",
+      receptor: "Marcelo",
+    },
+  ];
+
+  return (
+    <>
+      <Header />
+      <Container>
+        <Content>
+          <h1>Log de serviços</h1>
+          <Log>
+            <table>
+              <thead>
+                <tr>
+                  <th>Data</th>
+                  <th>Hora</th>
+                  <th>Emissor</th>
+                  <th>Receptor</th>
+                </tr>
+              </thead>
+              {logServicos.length > 0 ? (
+                <tbody>
+                  {logServicos.map((logServico) => (
+                    <tr>
+                      <td>{logServico.data}</td>
+                      <td>{logServico.hora}</td>
+                      <td>{logServico.emissor}</td>
+                      <td>{logServico.receptor}</td>
+                    </tr>
+                  ))}
+                </tbody>
+              ) : (
+                <tbody>
+                  <tr>
+                    <td>Não há</td>
+                  </tr>
+                </tbody>
+              )}
+            </table>
+          </Log>
+        </Content>
+      </Container>
+    </>
+  );
 }
