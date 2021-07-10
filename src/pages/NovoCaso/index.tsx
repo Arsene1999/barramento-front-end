4563338cac3cf00244d9acee548717edd331f047
João Ferreira dos Santos Neto <joaont.dev@gmail.com>
2021-07-10 13:52:13 -0300

    Adiciona formulario extra de configuracao

--- src/pages/NovoCaso/index.tsx
+++ src/pages/NovoCaso/index.tsx
@@ -1,44 +1,61 @@
 import { Container, Content, Inputs, Botao } from "./style";
 import { Header } from "../../components/Header";
-import { Link, useHistory } from 'react-router-dom';
+import { Link, useHistory } from "react-router-dom";
 import Voltar from "../../assets/Voltar.svg";
 import { useState } from "react";
 import api from "../../services/api";
 
+export function FormConfiguracao() {
+  return (
+    <>
+      <div>
+        <input placeholder="Nome da query" type="String"></input>
+        <input placeholder="Host" type="String"></input>
+      </div>
+      <input placeholder="Query" type="String"></input>
+      <div>
+        <input placeholder="Porta" type="String"></input>
+        <input placeholder="Usuário" type="String"></input>
+        <input placeholder="Senha" type="String"></input>
+      </div>
+    </>
+  );
+}
+
 export function NovoCaso() {
-  const [nameSistema,setNameSistema] = useState("");
+  const [nameSistema, setNameSistema] = useState("");
   const [descricao, setDescricao] = useState("");
   const [type, setType] = useState("");
   const history = useHistory();
-  
-  async function handleSubmit(e:any){
-    e.preventDefault();
-    api.defaults.headers.authorization = `Bearer ${localStorage.getItem('token')}`;
-    
-    try{
-      const res = await api.post('/project', {
-        "name": nameSistema,
-        "type": type,
-        "searchConfig": [ 
-          { 
-            "name": "aaaa",			
-            "query": "SELECT * FROM blabla.ble",
-            "host": "localhost",
-            "port": 3306,
-            "user": "root",
-            "password": "1234",
-            "description": "seila"
-          },
-        ]
-     });
 
-     history.push('/home')
-    }catch(e){
+  async function handleSubmit(e: any) {
+    e.preventDefault();
+    api.defaults.headers.authorization = `Bearer ${localStorage.getItem(
+      "token"
+    )}`;
+
+    try {
+      const res = await api.post("/project", {
+        name: nameSistema,
+        type: type,
+        searchConfig: [
+          {
+            name: "aaaa",
+            query: "SELECT * FROM blabla.ble",
+            host: "localhost",
+            port: 3306,
+            user: "root",
+            password: "1234",
+            description: "seila",
+          },
+        ],
+      });
+
+      history.push("/home");
+    } catch (e) {
       console.log(e);
     }
-
-  };
-
+  }
 
   return (
     <>
@@ -62,8 +79,9 @@ export function NovoCaso() {
             </div>
 
             <Inputs>
-              <input 
-                placeholder="Nome do Sistema" 
+              <h3>Sistema</h3>
+              <input
+                placeholder="Nome do Sistema"
                 type="String"
                 value={nameSistema}
                 onChange={(e) => setNameSistema(e.target.value)}
@@ -75,12 +93,14 @@ export function NovoCaso() {
                 value={descricao}
                 onChange={(e) => setDescricao(e.target.value)}
               ></input>
-              <input 
-                placeholder="Tipo" 
+              <input
+                placeholder="Tipo"
                 type="String"
                 value={type}
                 onChange={(e) => setType(e.target.value)}
               ></input>
+              <h3>Configurações do sistema</h3>
+              <FormConfiguracao />
               <div>
                 <button
                   style={{
